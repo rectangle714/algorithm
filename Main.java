d305449d801b698d3ec36a0aff32697a4318e007
rectangle714@naver.com <rectangle714@naver.com>
2024-04-04 08:11:21 +0900

    알고리즘 프로젝트 생성

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int A,B,C;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+
+    }
+}
