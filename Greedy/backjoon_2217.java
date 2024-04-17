b67841f65b65f09260fa376d55c6d74f565e4706
rectangle714@naver.com <rectangle714@naver.com>
2024-04-17 09:54:41 +0900

    백준 2217번[로프] 완료 - 20240417

---
+++ Greedy/backjoon_2217.java
@@ -0,0 +1,26 @@
+package Greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class backjoon_2217 {
+    /* 로프 */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Integer[] K = new Integer[N];
+
+        for(int i=0; i<N; i++) {
+            K[i] = sc.nextInt();
+        }
+        Arrays.sort(K, Comparator.reverseOrder());
+
+        for(int i=0; i<N; i++) {
+            K[i] = K[i] * (i+1);
+        }
+
+        int result = Arrays.stream(K).max(Comparator.comparingInt(o -> o)).get().intValue();
+        System.out.println(result);
+    }
+}
