58c783604a2604a42247a0e0e9eac43300e363e8
rectangle714@naver.com <rectangle714@naver.com>
2024-04-19 09:17:02 +0900

    백준 1541번[잃어버린 괄호] 완료 - 20240419

--- Greedy/baekjoon_1541.java
+++ Greedy/baekjoon_1541.java
@@ -3,27 +3,28 @@ package Greedy;
 import java.util.Scanner;
 
 public class baekjoon_1541 {
+    /* 잃어버린 괄호 */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
 
         int result = 0;
-        int currentIdx = 0;
-        String substring = "";
-        for(int i=0; i<line.length(); i++) {
-            if("+".equals(line.charAt(i)+"")) {
-                substring = line.substring(currentIdx, i);
-                result += Integer.parseInt(substring);
-                currentIdx = i+1;
+        String[] split = line.split("-");
+
+        for(int i=0; i<split.length; i++) {
+            if(split[i].contains("+")) {
+                int plusVal = 0;
+                String[] split1 = split[i].split("\\+");
+                for(String val : split[i].split("\\+")) {
+                    plusVal += Integer.parseInt(val);
+                }
+                split[i] = plusVal+"";
             }
-            if( "-".equals(line.charAt(i)+"")) {
-                substring = line.substring(currentIdx, i);
-                result += Integer.parseInt(substring);
-                currentIdx = i;
-            }
-            if(i == line.length()-1) {
-                substring = line.substring(currentIdx, line.length());
-                result += Integer.parseInt(substring);
+
+            if(i == 0) {
+                result += Integer.parseInt(split[i]);
+            } else {
+                result -= Integer.parseInt(split[i]);
             }
         }
 
