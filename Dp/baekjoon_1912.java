6671a791144d9b62d3dc9d1bb7baac4d82c92ed4
rectangle714@naver.com <rectangle714@naver.com>
2024-11-12 11:03:29 +0900

    백준 1912[연속합] - 20241112

---
+++ Dp/baekjoon_1912.java
@@ -0,0 +1,29 @@
+package Dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* 연속합 */
+public class baekjoon_1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxVal = arr[1];
+        for(int i=1; i<=n; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+            maxVal = Math.max(maxVal, dp[i]);
+        }
+
+        System.out.println(maxVal);
+    }
+}
