3940e8b421c74c82ed78dd79b93e4c71a3ef15ba
rectangle714@naver.com <rectangle714@naver.com>
2025-05-16 15:59:21 +0900

    [2025-05-16] 프로그래머스 [자연수뒤집어배열로만들기] 완료, [시저암호] 진행중

--- Dp/pgm_42898.java
+++ Dp/pgm_42898.java
@@ -9,10 +9,12 @@ public class pgm_42898 {
     }
 
     public static int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
+        int MOD_VALUE =  1_000_000_007;
         int[][] arr = new int[n][m];
+        // 시작점만 1로 채움
         arr[0][0] = 1;
 
+        // 배열의 웅덩이 위치에 -1로 값을 채움
         for(int[] puddle : puddles) {
             int y = puddle[0] - 1;
             int x = puddle[1] - 1;
@@ -22,16 +24,21 @@ public class pgm_42898 {
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
+                // 웅덩이가 있는 위치는 0으로 값을 변경
                 if(arr[i][j] == -1) {
                     arr[i][j] = 0;
                     continue;
                 }
 
-                if(i > 0) { arr[i][j] += arr[i-1][j] % 1000000007; }
-                if(j > 0) { arr[i][j] += arr[i][j-1] % 1000000007; }
+                /*
+                * 웅덩이가 없는 위치에는 왼쪽, 위 값을 더해주고,
+                * 계속 누적되어 값이 너무 커지는 것을 막기 위해 매번 나머지 값을 구해줌
+                */
+                if(i > 0) { arr[i][j] += arr[i-1][j] % MOD_VALUE; }
+                if(j > 0) { arr[i][j] += arr[i][j-1] % MOD_VALUE; }
             }
         }
 
-        return arr[n - 1][m - 1] % 1000000007;
+        return arr[n - 1][m - 1] % MOD_VALUE;
     }
 }
