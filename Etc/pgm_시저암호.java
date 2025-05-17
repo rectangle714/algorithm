01639bf174563aba687329fd80d91172ef1f6aae
rectangle714@naver.com <rectangle714@naver.com>
2025-05-18 00:12:27 +0900

    [2025-08-18] 프로그래머스 시저암호 완료

--- Etc/pgm_시저암호.java
+++ Etc/pgm_시저암호.java
@@ -4,23 +4,25 @@ public class pgm_시저암호 {
     public static void main(String[] args) {
         // 소문자 65 ~ 90
         // 대문자 97 ~ 122
-        System.out.println(solution("AaZz", 52));
+        System.out.println(solution("a B z", 4));
     }
 
     public static String solution(String s, int n) {
         String answer = "";
+
         for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == ' ') {
+            char c = s.charAt(i);
+            if(c == ' ') {
                 answer += " ";
                 continue;
             }
 
-            char c = s.charAt(i);
-
-            if('A' <= c && c <= 'Z') {
-                answer += (char)(c % 'Z' + n) ;
-            } else if('a' <= c && c <= 'z') {
-                answer += (char)(c % 'z' + n) ;
+            if('A' <= c && c <='Z') {
+                char value = (char)((c - 'A' + n) % 26 + 'A');
+                answer += value;
+            } else if('a' <= c && c <='z') {
+                char value = (char)((c - 'a' + n) % 26 + 'a');
+                answer += value;
             }
         }
 
