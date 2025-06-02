7cea4f61d2b9a9aefee180e8ca2ed0e4d22464e4
rectangle714@naver.com <rectangle714@naver.com>
2025-06-02 17:15:35 +0900

    [2025-06-02] 백준 [스택, 큐, 직각삼각형] 완료

---
+++ Etc/baekjoon_직각삼각형.java
@@ -0,0 +1,28 @@
+package Etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon_직각삼각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[3];
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+            Arrays.sort(arr);
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+
+            if ((arr[0] * arr[0]) + (arr[1] * arr[1]) == arr[2] * arr[2]) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        }
+    }
+}
