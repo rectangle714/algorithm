5e8995b771c910e46db8bc4b00427c19fcea8faf
rectangle714@naver.com <rectangle714@naver.com>
2025-06-10 15:41:39 +0900

    [2025-06-10] 백준 [쉬운최단거리, 최소힙] 완료

---
+++ Etc/baekjoon_최소힙.java
@@ -0,0 +1,29 @@
+package Etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class baekjoon_최소힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        for(int i=0; i<N; i++) {
+            int val = Integer.parseInt(br.readLine());
+            if(val == 0) {                  // 값이 0이면 출력
+                if(queue.isEmpty()) {       // 출력 상황에 비어있으면 0 출력
+                    System.out.println(0);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.offer(val);
+            }
+        }
+
+    }
+}
