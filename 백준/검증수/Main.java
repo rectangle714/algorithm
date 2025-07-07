785818edc763ac931a1db4e43d07c14b20851616
rectangle714@naver.com <rectangle714@naver.com>
2025-07-07 17:25:56 +0900

    [2025-07-07]

--- 백준/검증수/Main.java
+++ 백준/검증수/Main.java
@@ -10,8 +10,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i < 6; i++) {
-
+        long result = 0;
+        for(int i = 0; i < 5; i++) {
+            result += (long) Math.pow(Long.parseLong(st.nextToken()), 2);
         }
+
+        System.out.println(result % 10);
     }
 }
