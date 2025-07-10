8383883e10e688a133812d0f0dd15be358d74db6
rectangle714@naver.com <rectangle714@naver.com>
2025-07-10 17:21:47 +0900

    [2025-07-10]

--- 백준/벌집/Main.java
+++ 백준/벌집/Main.java
@@ -8,12 +8,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         if(n == 1) {
             System.out.println(1);
-        } else if(n >= 3) {
-            System.out.println((n % 6) + 1);
-        } else {
-            System.out.println((n / 6) + 1);
+            return;
         }
+
+        int val = 7;
+        int count = 2;
+        while(n > val) {
+            val += 6 * count;
+            count++;
+        }
+
+        System.out.println(count);
     }
 }
