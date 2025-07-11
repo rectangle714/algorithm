53cc4af1b78180398485fab336280378687db524
rectangle714@naver.com <rectangle714@naver.com>
2025-07-11 16:56:57 +0900

    [2025-07-11]

---
+++ 백준/팰린드롬수/Main.java
@@ -0,0 +1,28 @@
+package 백준.팰린드롬수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String s = br.readLine();
+            if(s.equals("0")) {
+                break;
+            }
+
+            String result = "yes";
+            for(int i = 0; i < s.length() / 2; i++) {
+                if(s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                    result = "no";
+                    break;
+                }
+            }
+
+            System.out.println(result);
+        }
+    }
+}
