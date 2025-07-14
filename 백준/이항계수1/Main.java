ff6c5199c7ee7459e72bee00985c92bdb228ab58
PHG <rectangle714@naver.com>
2025-07-14 16:32:09 +0900

    [2025-07-14]

---
+++ 백준/이항계수1/Main.java
@@ -0,0 +1,28 @@
+package 백준.이항계수1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n+1][k+1];
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= k; j++) {
+                if(j == 0 || i == 0 || i == j) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(arr[n][k]);
+    }
+}
